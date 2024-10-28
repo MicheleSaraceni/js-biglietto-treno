0836b7d793cf003bccaf2d621837c3c87349c87b
MicheleSaraceni <michele.saraceni23@email.com>
2024-10-28 16:20:29 +0100

    added allert km

--- script.js
+++ script.js
@@ -11,12 +11,17 @@ Buon lavoro! */
 
 const priceKm = 0.21;
 console.log ("Benvenuti sul nostri treno, la informiamo che la tariffa per viaggiare sulle nostre tratte è di: " + priceKm + " al km.");
+
 const juniorDiscount = 20;
 const juniorAge = 18;
 console.log ("Lo sconto del " + juniorDiscount + "% è applicato a tutti i biglietti acquistati da persone con un età inferiore ai " + juniorAge + "anni di età.");
+
 const seniorDiscount = 40;
 const seniorAge = 65;
 console.log ("Lo sconto del " + seniorDiscount + "% è applicato a tutti i biglietti acquistati da persone con un età uguale o superiore ai " + seniorAge + "anni di età.");
+
+const km = parseInt(prompt("Per acquistare un biglietto inserisca di seguito la lunghezza della tratta che deve percorre in km."));
+console.log ("La tratta che deve percorre è di " + km + "km");
 
 
 
